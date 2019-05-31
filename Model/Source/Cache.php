f9db4a793aba00a85277a050eeea3f8f02b3d661
Cédric Noel <gimli@dnd.fr>
2019-05-31 18:13:56 +0200

    reindex-cache-config: Create sources models

---
+++ Model/Source/Cache.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Pimgento\Api\Model\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Cache
+ *
+ * @category  Class
+ * @package   Cache
+ * @author    Agence Dn'D <contact@dnd.fr>
+ * @copyright 2019 Agence Dn'D
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://www.dnd.fr/
+ */
+class Cache implements ArrayInterface
+{
+    /**
+     * Cache options
+     *
+     * @var array $options
+     */
+    protected $options = [];
+    /**
+     * Magento cache list interface
+     *
+     * @var TypeListInterface $cacheTypeList
+     */
+    private $cacheTypeList;
+    /**
+     * This variable contain Logger
+     *
+     * @var LoggerInterface $logger
+     */
+    private $logger;
+
+    /**
+     * Cache constructor
+     *
+     * @param TypeListInterface $cacheTypeList
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        TypeListInterface $cacheTypeList,
+        LoggerInterface $logger
+    ) {
+        $this->cacheTypeList = $cacheTypeList;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Return cache options array
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        /** @var Magento\Framework\DataObject $types */
+        $types = $this->cacheTypeList->getTypes();
+
+        $this->options = [];
+        foreach ($types as $type) {
+            $this->options[] = ['value' => $type->getId(), 'label' => $type->getCacheType()];
+        }
+
+        return $this->options;
+    }
+}
---
+++ Model/Source/Indexer.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Pimgento\Api\Model\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+use Magento\Indexer\Model\Indexer\CollectionFactory;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Indexer
+ *
+ * @category  Class
+ * @package   Pimgento\Api\Model\Source
+ * @author    Agence Dn'D <contact@dnd.fr>
+ * @copyright 2019 Agence Dn'D
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://www.dnd.fr/
+ */
+class Indexer implements ArrayInterface
+{
+    /**
+     * Indexer options
+     *
+     * @var array $options
+     */
+    protected $options = [];
+    /**
+     * This variable contain Logger
+     *
+     * @var LoggerInterface $logger
+     */
+    private $logger;
+    /**
+     * This variable contains CollectionFactory
+     *
+     * @var CollectionFactory $indexerCollectionFactory
+     */
+    protected $indexerCollectionFactory;
+
+    /**
+     * Indexer constructor
+     *
+     * @param CollectionFactory $indexerCollectionFactory
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        CollectionFactory $indexerCollectionFactory,
+        LoggerInterface $logger
+    ) {
+        $this->indexerCollectionFactory = $indexerCollectionFactory;
+        $this->logger                   = $logger;
+    }
+
+    /**
+     * Return indexer options array
+     *
+     * @return string[]
+     */
+    public function toOptionArray()
+    {
+        /** @var \Magento\Indexer\Model\Indexer\Collection $indexerCollection */
+        $indexerCollection = $this->indexerCollectionFactory->create();
+        /** @var string[] $indexerIds */
+        $indexerIds = $indexerCollection->getAllIds();
+
+        $this->options = [];
+        foreach ($indexerIds as $indexId) {
+            $this->options[] = ['value' => $indexId, 'label' => $indexId];
+        }
+
+        return $this->options;
+    }
+}
